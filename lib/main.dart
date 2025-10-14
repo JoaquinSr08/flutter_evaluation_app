81ba7363a65b0e83c9d7c3a254eae649868b3dad
NicolasAVM96 <nicolas.AVM96@gmail.com>
2025-10-14 20:06:55 -0300

    Se sube el proyecto a firebase y se realizar todas las configuraciones necesarias

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:task_evaluations_app/firebase_options.dart';
 import 'screens/login_screen.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const TaskEvaluationsApp());
 }
 
